42e78f507927c2245d88cc0cb0afa0f2684b7cac
pipipupucheeeck <genadijpetrov372@gmail.com>
2025-08-28 16:27:57 +0000

    Initial commit

---
+++ SampleNewProject/Main.java
@@ -0,0 +1,10 @@
+//if you want to start new projects in new folders, add the line below (THIS MUST MATCH THE NAME OF THE FOLDER)
+package SampleNewProject;
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        System.out.println("This is a sample project to show how to start a new project");
+    }
+}
